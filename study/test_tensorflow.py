98d941f391a23e56948f2ef6084678b46065f41d
Altas <57321786+ChenYPeng@users.noreply.github.com>
2022-07-31 16:39:01 +0800

    Delete study directory

--- study/test_tensorflow.py
+++
@@ -1,9 +0,0 @@
-# 验证tensorflow
-import tensorflow as tf
-print(tf.__version__)
-# 验证cuda
-a = tf.test.is_built_with_cuda()
-print(a)
-
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-print(physical_devices)
